e77d7a59a8a6a5cd4385221e66212391a67b8e09
estebanfern <estebangfernandeza@gmail.com>
2022-12-22 15:08:57 -0300

    HTML y CSS terminados

---
+++ calculadora/main.js
@@ -0,0 +1,7 @@
+function addNumber(num) {
+    document.getElementById("ds").value = parseFloat(document.getElementById("ds").value + num); 
+}
+
+function clear() {
+    document.getElementById("ds").value = 0;
+}
